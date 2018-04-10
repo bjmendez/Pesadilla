103f7dd31b183d87d43c393335f3844712076c3f
Arody Deleon <arody888@yahoo.com>
2018-04-10 02:27:34 -0400

    Health Drop to 50%

---
+++ Assets/Pesadilla_Data/Scripts/HealthDrop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDrop : MonoBehaviour {
+    private GameObject Player;
+
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindWithTag("Player");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Player.SendMessage("AddHealth", 10);
+            Destroy(gameObject);
+        }
+    }
+}
